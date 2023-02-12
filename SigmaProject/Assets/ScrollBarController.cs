7e7ef6cc1e1ebbdd8b54a67141714f8dfa1a0f71
Spakelios <73300254+Spakelios@users.noreply.github.com>
2023-02-12 19:14:26 +0000

    Basic database interface recreation, with minor thematic changes

---
+++ SigmaProject/Assets/ScrollBarController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollBarController : MonoBehaviour
+{
+    public Scrollbar SB;
+    public GameObject Panel;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        {
+            
+        }
+    }
+}
