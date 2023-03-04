83ebc7784ca60c4b6a76b545911a51298074b424
Spakelios <73300254+Spakelios@users.noreply.github.com>
2023-03-04 18:46:45 +0000

    prototype hub world animations and small bit of polish :D

--- SigmaProject/Assets/Kellies Stuff/Code/ButtonController.cs
+++ SigmaProject/Assets/Kellies Stuff/Code/ButtonController.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -8,8 +10,8 @@ using UnityEngine.UI;
 public class ButtonController : MonoBehaviour
 {
     public GameObject Cam1, Cam2, player, text, PB;
-    
-    public Animator anim, ShaderAnim;
+    public walk walk;
+    public Animator anim, ShaderAnim, StoneAnim;
     public GameObject popup;
     
     public void ActivateScroll()
@@ -50,5 +52,28 @@ public class ButtonController : MonoBehaviour
     {
         PB.SetActive(true);
         anim.Play("Interactive");
+    }
+
+
+    public void ChooseABook()
+    {
+        StartCoroutine(CAB());
+    }
+
+    private IEnumerator CAB()
+    {
+        walk.speed = 0;
+        
+        anim.Play("NoBooks");
+
+        yield return new WaitForSeconds(10f);
+        
+        Cam2.SetActive(true);
+        Cam1.SetActive(true);
+        StoneAnim.Play("MovingPath");
+
+        yield return new WaitForSeconds(7f);
+
+        walk.speed = 10;
     }
 }
